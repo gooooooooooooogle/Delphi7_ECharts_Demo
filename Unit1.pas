e694f3b83362ba5aaa9231504c6f43815a1af0d8
gooooooooooooogle <1185025336@qq.com>
2021-11-04 14:20:40 +0800

    init

--- Unit1.pas
+++ Unit1.pas
@@ -32,17 +32,17 @@ i : Integer;
 PathStr : string;
 begin
    sl := TStringList.Create;
-   sl.LoadFromFile('./chart.html'); //38、106 行 为数据
+   sl.LoadFromFile('./template.html'); //38、106 行 为数据
 
    sl[38] := 'data: [67, 529, 5207, 24674, 73680, 110000, 22030, 995, 78, 73, 0],';
    sl[106] := 'data: [0, 529, 5207, 20000, 60000, 80000, 20000, 995, 78, 73, 0],';
 
    //生成并保存网页
-   PathStr := ExtractFilePath(Application.ExeName) + 'chart.html';
+   PathStr := ExtractFilePath(Application.ExeName) + 'show.html';
    sl.SaveToFile(PathStr);
 
    //加载本地网页
-   wb1.Navigate(ExtractFileDir(ParamStr(0)) + '\chart.html');
+   wb1.Navigate(ExtractFileDir(ParamStr(0)) + '\show.html');
 end;
 
 end.
